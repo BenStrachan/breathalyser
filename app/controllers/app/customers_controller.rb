5fb71d915a7b26b89a62ba19bc484d5906e87744
chutuan <chutuan020693@gmail.com>
2018-03-02 11:44:30 +0700

    Fix theme

--- app/controllers/app/customers_controller.rb
+++ app/controllers/app/customers_controller.rb
@@ -29,7 +29,7 @@ class App::CustomersController < ApplicationController
 
     respond_to do |format|
       if @customer.save
-        format.html { redirect_to @customer, notice: 'Customer was successfully created.' }
+        format.html { redirect_to app_customers_path, notice: 'Customer was successfully created.' }
         format.json { render :show, status: :created, location: @customer }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class App::CustomersController < ApplicationController
   def update
     respond_to do |format|
       if @customer.update(customer_params)
-        format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
+        format.html { redirect_to app_customers_path, notice: 'Customer was successfully updated.' }
         format.json { render :show, status: :ok, location: @customer }
       else
         format.html { render :edit }
@@ -57,7 +57,7 @@ class App::CustomersController < ApplicationController
   def destroy
     @customer.destroy
     respond_to do |format|
-      format.html { redirect_to customers_url, notice: 'Customer was successfully destroyed.' }
+      format.html { redirect_to app_customers_path, notice: 'Customer was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
