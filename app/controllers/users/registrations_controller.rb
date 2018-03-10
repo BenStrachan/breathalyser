b8f97a36e29e059734bc42131f7102a0848b7c37
chutuan <chutuan020693@gmail.com>
2018-03-10 13:05:17 +0700

    Fix bugs

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,14 +3,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    redirect_to root_path
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    redirect_to root_path
+  end
 
   # GET /resource/edit
   # def edit
