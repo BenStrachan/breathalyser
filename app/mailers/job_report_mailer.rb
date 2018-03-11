b4914bae33f951464587579684489e346ed4ecbd
chutuan <chutuan020693@gmail.com>
2018-03-11 11:29:20 +0700

    Update

--- app/mailers/job_report_mailer.rb
+++ app/mailers/job_report_mailer.rb
@@ -2,17 +2,7 @@ class JobReportMailer < ApplicationMailer
   def send_certificate(job_report)
     @job_report = job_report
 
-    attachments["Certificate.pdf"] =
-      WickedPdf.new.pdf_from_string(
-        render_to_string(
-          pdf: "Certificate",
-          file: 'pdfs/certificate',
-          locals: {
-            job_report: job_report
-          }
-        )
-      )
-    
+
     return false if job_report.job.customer&.email.blank?
     
     mail(
