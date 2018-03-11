5026b7350a71984f64ff48d6f4274d5dab757b91
chutuan <chutuan020693@gmail.com>
2018-03-11 10:56:22 +0700

    Add content to certificate

--- app/mailers/job_report_mailer.rb
+++ app/mailers/job_report_mailer.rb
@@ -1,5 +1,7 @@
 class JobReportMailer < ApplicationMailer
   def send_certificate(job_report)
+    @job_report = job_report
+
     attachments["Certificate.pdf"] =
       WickedPdf.new.pdf_from_string(
         render_to_string(
